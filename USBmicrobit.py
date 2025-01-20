173f84397854cfcbce09090508fcf1eee05d24e5
AllClouded <thomas.batchelor10@icloud.com>
2025-01-20 13:56:35 +0000

    .

---
+++ USBmicrobit.py
@@ -0,0 +1,20 @@
+import serial
+
+
+
+import serial.tools.list_ports as port_list
+ports = list(port_list.comports())
+for p in ports:
+    print (p)
+
+
+try:
+    port = serial.Serial('/dev/ttyS1', 115200)
+
+    print("Started")
+
+    while True:
+        message = port.readline().decode().strip()
+        print(message)
+except:
+    print("failed")
